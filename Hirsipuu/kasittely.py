b72d0261d4a2e9d2b00279b5fc32a0f686b4ca0f
JusaTiVi <jusa.itkonen@esedulainen.fi>
2026-01-20 11:36:48 +0200

    Add files via upload

--- Hirsipuu/kasittely.py
+++ Hirsipuu/kasittely.py
@@ -1,8 +1,14 @@
 import random
 tiedosto = "sanat.txt"
+pistelista = "pisteet.txt"
 class Hirsipuu:
     def __init__(self, tiedosto):
         self.tiedosto = tiedosto
+        self.nimi = nimi
+        self.pisteet = 0
+        self.uusi_peli()
+    
+    def uusi_peli(self):
         self.sana = self.arpa()
         self.vaarat = 0
         self.oikeat = []
@@ -12,10 +18,11 @@ class Hirsipuu:
     def arpa(self):
         with open(self.tiedosto) as file:
             sanat = file.read().split()
-        return random.choice(sanat)
+        sana = random.choice(sanat)
+        return sana
 
     def kysy_kirjain(self):
-        return input("Anna kirjain: ")
+        return input("Anna kirjain: ").lower()
 
     def tarkista_kirjain(self, kirjain):
         if kirjain not in self.sana:
@@ -23,18 +30,26 @@ class Hirsipuu:
         else:
             if kirjain not in self.oikeat:
                 self.oikeat.append(kirjain)
+            else:
+                return print("kirjain on jo löydetty!")
 
     def tarkista_havio(self):
         if self.vaarat >= self.max_vaarat:
             print("Hävisit pelin!")
+            print("sana oli:", self.sana)
             return True
         return False
 
     def tarkista_voitto(self):
         if len(self.oikeat) == len(self.kirjaimet):
             print("Voitit pelin!")
+            self.pisteet += 1
+            self.t
             return True
         return False
+    def tallenna_pisteet(self):
+        with open(pistelista) as tiedosto:
+            tiedosto.write(f"{self.nimi}: {self.pisteet}\n")
 
     def pelaa(self):
         while True:
@@ -45,6 +60,9 @@ class Hirsipuu:
                 break
             if self.tarkista_voitto():
                 break
-            
-peli = Hirsipuu(tiedosto)
-peli.pelaa()
+nimi = input("Anna pelaajan nimi: ")
+peli = Hirsipuu(tiedosto, nimi)
+
+while True:
+    peli.pelaa()
+    peli.uusi_peli()
